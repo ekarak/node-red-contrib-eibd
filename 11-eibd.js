37324e99a8a920bfce9ddc0eb029455d148081ed
Elias Karakoulakis <elias.karakoulakis@gmail.com>
2014-07-19 11:08:37 +0300

    caution: wet floor

--- 11-eibd.js
+++ 11-eibd.js
@@ -23,98 +23,116 @@ module.exports = function(RED) {
 	
 	var eibd = require('eibd');
 
-	var listeners = {};
-	listeners['dest'] = {}; // hashmap of listeners by telegram's destination
-	listeners['dpt'] = {}; // hashmap of listeners by telegram's datapoint type
-	listeners['src'] = {}; // hashmap of listeners by telegram's source (PA or GA)
-	
-	/**
-	* send a group write telegram to a group address (see bin/groupswrite and bin/groupwrite)
-	* opts: { host: localhost, port: 6720}
-	* gad: group address '1/2/34'
-	* width: DPT bits width (derived from datatype)
-	* value: the value to write
-	* callback: 
-	*
-	* Usage:   
-	* groupAddrWrite({ host: 'localhost', port: 6720}, '1/2/34', 1, function(err) {
-    *   if(err) console.error(err);
-    * });
-	*/
-	function groupAddrWrite(opts, gad, width, value, callback) {
-	  console.log('groupAddrWrite');
-	  var conn = eibd.Connection();
-	  var address = eibd.str2addr(gad);
-	  var data;
-	  conn.socketRemote(opts, function() {
-		conn.openTGroup(address, 1, function (err) {
-		  if(err && (typeof callback === 'function')) {
-		  	  callback(err) 
-		  } else {
-		  	// most common case
-		  	if (width <= 6) {
-		  		data = new Array(2);
-		  	  	data[0] = 0;
-		  	  	data[1] = 0x80 | value;
-			} else {
-				data = new Array(3);
-				data[0] = 0;
-				data[1] = 0x80; 
-				data[2] = (0xff & value);
-				// TODO: what about strings?
-			}
-			conn.sendAPDU(data, callback);
-		  }
-		});
-	  });
-	}
-
 	function EibdControllerNode(config) {
-		console.log("new EibdControllerNode");
-		RED.nodes.createNode(this,config);
+		console.log("new EibdControllerNode, config: %j", config);
+		RED.nodes.createNode(this, config);
 		this.host = config.host;
 		this.port = config.port;
 		var node = this;
-		//
-		this.sendTelegram = function(gad, width, value) {
-			groupAddrWrite({host: this.host, port: this.port}, gad, width, value, null);
+		this.eibdSendConn = null;
+
+		/**
+		* send a group write telegram to a group address (see bin/groupswrite and bin/groupwrite)
+		* Must be called in the context of a _valid_ eibd connection.
+		* dstgad: dest group address '1/2/34'
+		* dpt: DataPointType eg. '1' for boolean 
+		* value: the value to write
+		* callback: 
+		*
+		* Usage:   
+		* groupAddrWrite({ host: 'localhost', port: 6720}, '1/2/34', 1, 1, function(err) {
+		*   if(err) console.error(err);
+		* });
+		*/
+		this.groupAddrWrite = function(dstgad, value, dpt, callback) {
+			console.log('groupAddrWrite opts:%s, gad:%s, dpt:%s, value:%s', opts, dstgad, dpt, value);
+			var address = eibd.str2addr(dstgad);
+			eibdSendConn.openTGroup(address, 1, function (err) {
+				if(err && (typeof callback === 'function')) {
+					console.log('error calling openTGroup!: %j', err);
+					callback(err);
+				} else {
+					var data;
+					console.log("in else");
+					// most common case
+					if (dpt === '1') {
+						data = new Array(2);
+						data[0] = 0;
+						data[1] = 0x80 | value;
+					} else {
+						data = new Array(3);
+						data[0] = 0;
+						data[1] = 0x80; 
+						data[2] = (0xff & value);
+					// TODO: what about strings?
+					}
+					console.log("sendAPDU");
+					eibdSendConn.sendAPDU(data, callback);
+				}
+			});
 		}
+		
+		/**
+		* 
+		*/
+		this.sendTelegram = function(dstgad, value, dpt) {
+			if ( typeof eibdSendConn != 'undefined' && eibdSendConn ) {				
+				this.groupAddrWrite(dstgad, value, dpt, function(err) {
+					if (err) {
+						console.log('groupAddrWrite error: %j', err);
+						//setTimeout(this.sendTelegram, 10000, dstgad, value, dpt);
+					}
+				});
+			}
+		}
+
 		/* ===== Node-Red events ===== */
 		this.on("error", function(msg) {
 			
 		});
-		//
 		/* ===== eibd events ===== */
-		function groupsocketlisten(opts, callback) {
-			var conn = eibd.Connection();
-			conn.socketRemote(opts, function() {
-					conn.openGroupSocket(0, callback);
-			});
-		};
-		//
-		groupsocketlisten({ host: n.host, port: n.port }, function(parser) {
-			//
+		var registerParsers = function(parser) {
 			parser.on('write', function(src, dest, dpt, val){
-				node.log('Write from '+src+' to '+dest+': '+val);
-				var msg = {knxtype: 'write', src: src, dest: desc, dpt: dpt, val: val};
-				for (nrn in listeners['dest'][dst]) nrn.emit(msg);
-				for (nrn in listeners['dpt'][dpt]) nrn.emit(msg);
-				for (nrn in listeners['src'][src]) nrn.emit(msg);
+				console.log('Write from '+src+' to '+dest+': '+val);
+				var msg = {knxtype: 'write', srcphy: src, dstgad: dest, dpt: dpt, value: val};
+				emit(msg);
 			});
 			//
 			parser.on('response', function(src, dest, val) {
-				node.log('Response from '+src+' to '+dest+': '+val);
-				var msg = {knxtype: 'response', src: src, dest: desc, val: val};
-				for (nrn in listeners['dest'][dst]) nrn.emit(msg);
-				for (nrn in listeners['src'][src]) nrn.emit(msg);
+				console.log('Response from %s to %s: %s', src, dest, val);
+				var msg = {knxtype: 'response', srcphy: src, dstgad: dest, value: val};
+				emit(msg);
 			});
 			//
 			parser.on('read', function(src, dest) {
-				node.log('Read from '+src+' to '+dest);
-				var msg = {knxtype: 'read', src: src, dest: desc, val: val};
-				for (nrn in listeners['dest'][dst]) nrn.emit(msg);
-				for (nrn in listeners['src'][src]) nrn.emit(msg);
+				console.log('Read from %s to %s', src, dest);
+				var msg = {knxtype: 'read', srcphy: src, dstgad: desc, value: val};
+				emit(msg);
 			});
+		};
+		/**
+		* Initialize an eibd socket, calling the handler function 
+		* when successfully connected, passing it the eibd connection
+		*/
+		var initializeEibdSocket = function (handler) {
+			var eibdconn = new eibd.Connection();
+			console.log('connecting to eibd at %s:%d...', config.host, config.port);
+			eibdconn.socketRemote({ host: config.host, port: config.port }, function(err) {
+				if (err) {
+					console.log('eibd.socketRemote error: %s', err.code);
+					setTimeout(initializeEibdSocket, 10000);
+				} else {
+					handler(eibdconn);
+				}
+			});
+		};
+		// initialize incoming KNX event socket (openGroupSocket)
+		initializeEibdSocket(function(eibdconn) { 
+			eibdconn.openGroupSocket(0, registerParsers); 
+		});
+		// initialize outgoing KNX telegram socket (openTGroup)
+		initializeEibdSocket(function(eibdconn) {
+			this.eibdSendConn = eibdconn;
 		});
 	}
 	//
@@ -124,24 +142,27 @@ module.exports = function(RED) {
 		console.log("new EibdOut");
 		RED.nodes.createNode(this, config);
 		this.name = config.name;
-		var node = this;
+		//var node = this;
 		var eibdController = RED.nodes.getNode(config.controller);
 		//
 		this.on("input", function(msg) {
+			console.log('eibdout.onInput');
 			if (msg != null) {
 				switch(true) {
 				case /read/.test(msg.topic):
 					break; // TODO
 				case /respon/.test(msg.topic):
 					break; // TODO
-				else:
+				default:
 					eibdController.sendTelegram(
 						msg.payload.groupaddr,
 						msg.payload.width,
 						msg.payload.value
 					);
+				}
 			};
 		});
+		console.log("new EibdOut - done");
 	}
 	//
 	RED.nodes.registerType("eibd-out", EibdOut);
@@ -161,4 +182,4 @@ module.exports = function(RED) {
 	}
 	//
 	RED.nodes.registerType("eibd-in", EibdIn);
-}
+} 	
